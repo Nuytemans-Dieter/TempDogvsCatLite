c3494adb903381653eb81cc8979458ef577e6651
Dieter Nuytemans <37397560+Nuytemans-Dieter@users.noreply.github.com>
2021-01-17 11:18:53 +0100

    Update run.py

--- run.py
+++ run.py
@@ -27,5 +27,4 @@ classes = classify.get_classes(interpreter, top_k=1)
 
 # Print the result
 for c in classes:
-  print(c.id)
-  print('%s: %.5f' % (labels[c.id], c.score))
+  print('%s: %.5f' % (c.id, c.score))
